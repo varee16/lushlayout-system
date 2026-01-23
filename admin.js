828204cd0b0209f7b84e714b5b07ddd050e88615
Waree Boonsanong <Waree.B@roki-thailand.com>
2026-01-23 10:08:46 +0700

    update admin.js

--- admin.js
+++ admin.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
+const path = require("path")
 
 router.get("/", (req,res) => {
     if(req.query.key !== process.env.ADMIN_SECRET)
         return res.send("Unauthorized");
 
-    const orders = JSON.parse(fs.readFileSync("data/orders.json"));
-    res.json(orders);
+    const ordersPath = path.join(__dirname, "../data/orders.json");
+
+    if(!fs.existsSync(ordersPath)) {
+        return res.json({success: true, orders: []})
+    }
+
+    const orders = JSON.parse(fs.readFileSync(ordersPath, "utf-8"));
+
+
+    res.json({
+        success: true,
+        orders
+    })
 });
 
 module.exports = router;
