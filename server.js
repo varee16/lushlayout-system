cef6f0762f4a9279f49a96df8caee912e169ef5e
Waree Boonsanong <Waree.B@roki-thailand.com>
2026-01-23 08:46:06 +0700

    edit file orders.json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const fs = require("fs")
-const { error } = require("console")
+
 
 const app = express()
 app.get("/", (req, res) => {
